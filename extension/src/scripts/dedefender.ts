7037e31e4f781ffd3d5e164d20ae05e6e038b107
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-08-22 10:54:03 +0200

    :sparkles: Also remove click handler of MS and optimize link handling

--- extension/src/scripts/dedefender.ts
+++ extension/src/scripts/dedefender.ts
@@ -4,28 +4,45 @@ const dedefenderService = new DeDefenderService()
 
 const findDefenderLinks = (): HTMLAnchorElement[] => {
   const anchorElements = Array.from(document.getElementsByTagName("a"))
-  return anchorElements.filter(element => {
-    if (element.href.length > 0) {
-      const url = new URL(element.href)
-      return url.host.endsWith("mcas-proxyweb.mcas.ms")
-    } else {
-      return false
-    }
-  })
+  return anchorElements.filter(element => filterForValidLinks(element))
+}
+
+const filterForValidLinks = (anchor: HTMLAnchorElement): boolean => {
+  if (anchor.href.length > 0) {
+    const url = new URL(anchor.href)
+    return url.host.endsWith("mcas-proxyweb.mcas.ms")
+  } else {
+    return false
+  }
 }
 
 const dedefendLinkNode = (linkElement: HTMLAnchorElement) => {
+  if (!filterForValidLinks(linkElement)) {
+    return
+  }
   const input = linkElement.href
   const unwrap = dedefenderService.dedefendUrl(input)
   linkElement.href = unwrap
   console.debug(`DeDefend: ${input} -> ${unwrap}`)
 }
 
+const removeMSLinkListener = (linkElement: HTMLAnchorElement) => {
+  // replace element with a clone to discard click handlers
+  const anchorClone = linkElement.cloneNode(true)
+  linkElement.replaceWith(anchorClone)
+}
+
 const links = findDefenderLinks()
 links.forEach(anchor => dedefendLinkNode(anchor))
 
-const observer = new MutationObserver(() => {
-  const links = findDefenderLinks()
-  links.forEach(anchor => dedefendLinkNode(anchor))
+const observer = new MutationObserver(mutations => {
+  mutations
+    .filter(mutation => mutation.target.nodeName.toLowerCase() == "a")
+    .filter(mutation => filterForValidLinks(mutation.target))
+    .forEach(async (mutation) => {
+      const element: HTMLAnchorElement = mutation.target
+      dedefendLinkNode(element)
+      removeMSLinkListener(element)
+    })
 })
-observer.observe(window.document.body, {attributes: true, attributeFilter: ["href"], childList: true, subtree: true})
+observer.observe(window.document.body, {attributes: true, childList: true, subtree: true})
