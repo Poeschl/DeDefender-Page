62a7995c0bbf8c742d39e28aa896986feb835bf3
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-08-23 11:20:31 +0200

    :sparkles: Work around Teams iframe containers

--- extension/src/scripts/dedefender.ts
+++ extension/src/scripts/dedefender.ts
@@ -2,15 +2,31 @@ import DeDefenderService from "../../../src/service/DeDefenderService";
 
 const dedefenderService = new DeDefenderService()
 
+const getPageRootBody = (): HTMLElement => {
+  //check for electron container
+  const possibleIframeWrappers = document.getElementsByClassName("embedded-electron-webview")
+  if (possibleIframeWrappers.length > 0) {
+    const iframeWrapper = possibleIframeWrappers[0]
+    if (iframeWrapper !== null && iframeWrapper.nodeName.toLowerCase() === "iframe") {
+      // @ts-ignore: Object is possibly 'null'.
+      return (iframeWrapper as HTMLIFrameElement).contentDocument.body
+    } else {
+      return window.document.body
+    }
+  } else {
+    return window.document.body
+  }
+}
+
 const findDefenderLinks = (): HTMLAnchorElement[] => {
-  const anchorElements = Array.from(document.getElementsByTagName("a"))
+  const anchorElements = Array.from(getPageRootBody().getElementsByTagName("a"))
   return anchorElements.filter(element => filterForValidLinks(element))
 }
 
 const filterForValidLinks = (anchor: HTMLAnchorElement): boolean => {
   if (anchor.href.length > 0) {
     const url = new URL(anchor.href)
-    return url.host.endsWith("mcas-proxyweb.mcas.ms")
+    return url.host.endsWith("mcas-proxyweb.mcas.ms") && anchor.getAttribute("dedefended") !== "true" && anchor.target == "_blank"
   } else {
     return false
   }
@@ -38,21 +54,31 @@ const handleAnchor = (anchor: HTMLAnchorElement) => {
   removeMSLinkListener(anchor)
 }
 
-const links = findDefenderLinks()
-links.forEach(anchor => handleAnchor(anchor))
-
-const observer = new MutationObserver(mutations => {
+const anchorObserverFunction = (mutations: MutationRecord[]) => {
   mutations
     .flatMap<Node>(mutation => {
       if (mutation.type == "childList") {
-        return Array.from(mutation.addedNodes).filter(element => element.nodeName.toLowerCase() == "a")
-      } else if (mutation.type == "attributes" && mutation.target.nodeName.toLowerCase() == "a") {
+        return Array.from(mutation.addedNodes).filter(element => element.nodeName.toLowerCase() === "a")
+      } else if (mutation.type == "attributes" && mutation.target.nodeName.toLowerCase() === "a") {
         return [mutation.target]
       }
       return []
     })
     .map(mutation => mutation as HTMLAnchorElement)
-    .filter(anchor => anchor.getAttribute("dedefended") !== "true")
-    .forEach(element => handleAnchor(element))
-})
-observer.observe(window.document.body, {childList: true, subtree: true})
+    .filter(anchor => filterForValidLinks(anchor))
+    .forEach(element => {
+      handleAnchor(element)
+    })
+  findDefenderLinks().forEach(anchor => {
+    console.debug(`Detected leftover anchor "${anchor.href}"`)
+    handleAnchor(anchor)
+  })
+}
+
+
+const links = findDefenderLinks()
+links.forEach(anchor => handleAnchor(anchor))
+
+const observer = new MutationObserver(anchorObserverFunction)
+console.debug(`Detected page root: ${JSON.stringify(getPageRootBody())}`)
+observer.observe(getPageRootBody(), {attributes: true, attributeFilter: ["href"], childList: true, subtree: true})
